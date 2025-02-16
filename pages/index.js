78fa421418fb4ca6a23d0c2f2e3abc4bba1ba195
Meabeab <malikovigor2004@mail.ru>
2025-02-16 19:34:01 +0400

    commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+    const logInBtn = document.querySelector('.navbar__sign-btn_log-in');
+    const signUpBtn = document.querySelector('.navbar__sign-btn_reg-in');
+    const signContainer = document.querySelector('.sign');
+    const logInForm = document.querySelector('.sign-in');
+    const signUpForm = document.querySelector('.sign-up');
+    const hintBtn = document.querySelector('.sign__hint-btn');
+
+    const showPopup = (form) => {
+        signContainer.classList.add('sign_active');
+        form.classList.add('sign_active');
+        navbar.classList.add('navbar_disactive');
+    };
+
+    const hidePopups = () => {
+        signContainer.classList.remove('sign_active');
+        logInForm.classList.remove('sign_active');
+        signUpForm.classList.remove('sign_active');
+        navbar.classList.remove('navbar_disactive');
+    };
+
+    logInBtn.addEventListener('click', () => {
+        hidePopups();
+        showPopup(logInForm);
+    });
+
+    signUpBtn.addEventListener('click', () => {
+        hidePopups();
+        showPopup(signUpForm);
+    });
+
+    hintBtn.addEventListener('click', () => {
+        logInForm.classList.remove('sign_active');
+        showPopup(signUpForm);
+    });
+
+    signContainer.addEventListener('click', (event) => {
+        if (event.target === signContainer) {
+            hidePopups();
+        }
+    });
+});
