a0d6b6d08e97a401059950c2a653349810fbd125
Meabeab <malikovigor2004@mail.ru>
2025-02-23 20:05:34 +0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -33,25 +33,25 @@ document.addEventListener('DOMContentLoaded', () => {
         signUpForm.classList.remove('sign_active');
         navbar.classList.remove('navbar_disactive');
     };
-    logInBtn.addEventListener('click', () => {
+    logInBtn?.addEventListener('click', () => {
         hidePopups();
         showPopup(logInForm);
     });
-    signUpBtn.addEventListener('click', () => {
+    signUpBtn?.addEventListener('click', () => {
         hidePopups();
         showPopup(signUpForm);
     });
-    hintBtn.addEventListener('click', () => {
+    hintBtn?.addEventListener('click', () => {
         logInForm.classList.remove('sign_active');
         showPopup(signUpForm);
     });
-    signContainer.addEventListener('click', (event) => {
+    signContainer?.addEventListener('click', (event) => {
         if (event.target === signContainer) {
             hidePopups();
         }
     });
     // promo
-    promoButton.addEventListener("click", () => {
+    promoButton?.addEventListener("click", () => {
         if (promoVideo.paused) {
             promoVideo.play();
             promoButtonText.innerHTML = "Pause the video";
@@ -61,13 +61,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
     // novelties
-    novelties.forEach((novelties) => {
-        novelties.addEventListener("click", () => {
-            if (novelties.classList.contains("novelties__card_active")) return;
-            document.querySelector(".novelties__card_active")?.classList.remove("novelties__card_active");
-            novelties.classList.add("novelties__card_active");
+    function handleNovelties() {
+        if (window.innerWidth < 1440) return;
+        
+        novelties.forEach((novelty) => {
+            novelty.addEventListener("click", () => {
+                if (novelty.classList.contains("novelties__card_active")) return;
+                document.querySelector(".novelties__card_active")?.classList.remove("novelties__card_active");
+                novelty.classList.add("novelties__card_active");
+            });
         });
+    }
+    handleNovelties();
+    window.addEventListener("resize", () => {
+        if (window.innerWidth < 1440) {
+            document.querySelectorAll(".novelties__card_active").forEach(el => el.classList.remove("novelties__card_active"));
+        } else {
+            handleNovelties();
+        }
     });
+
     // like
     likeButtons.forEach((button) => {
         button.addEventListener("click", (event) => {
@@ -75,6 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
             button.classList.toggle("featured__card-like-active");
         });
     });
+
     // faq
     faqItems.forEach((item) => {
         const question = item.querySelector(".faq__question");
